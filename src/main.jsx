1784f1b58c79024eef5566395aac5baaae65b113
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-13 01:28:46 +0530

    Activate final interaction fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './mindmate-final-fix.js'
+import './mindmate-interaction-fix.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

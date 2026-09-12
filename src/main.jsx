787498c32e829c8068f87c5ac817f6abf57bf09b
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-13 01:05:53 +0530

    Use final UI fixes without conflicting toolbar patches

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './stickerToolbarFix.js'
-import './mindmate-ui-fixes.js'
+import './mindmate-final-fix.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

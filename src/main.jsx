e0a041f35e4451dd6d6a58deae0c067dca34b4c7
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-13 01:29:04 +0530

    Activate template card fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './mindmate-final-fix.js'
 import './mindmate-interaction-fix.js'
+import './mindmate-card-fix.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

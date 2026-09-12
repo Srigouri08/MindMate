872c8c3ddb31d4cd9df595fa3deaf22c685eb85e
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-13 00:29:55 +0530

    Load toolbar and template fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './stickerToolbarFix.js'
+import './mindmate-ui-fixes.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

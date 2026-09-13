570271eb8728fd8991b93a7ed8c7e7756242b935
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-13 13:39:18 +0530

    Add wellness tracker fixes

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import './mindmate-final-fix.js'
 import './phaseOne.js'
 import './themeFix.js'
+import './wellnessFix.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

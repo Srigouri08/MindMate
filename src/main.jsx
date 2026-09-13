5d116eb457df011876efcb1d89ca121fdec79d67
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-13 15:02:22 +0530

    Restore existing MindMate features safely

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './mindmate-final-fix.js'
+import './phaseOne.js'
+import './themeFix.js'
+import './wellnessFix.js'
+import './wellnessPolish.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

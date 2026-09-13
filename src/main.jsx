15d93951ee31f436118c7eda21f7c588b9b568e5
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-13 15:37:23 +0530

    Enable journal tools and sticker interactions

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './mindmate-theme.css'
+import './mindmate-ui-fixes.js'
+import './mindmate-interaction-fix.js'
+import './stickerToolbarFix.js'
+import './mindmate-final-fix.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

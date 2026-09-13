4b6983081bf2e24de15786a605b163e7660c5243
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-13 22:03:20 +0530

    Enable left sidebar feature controls

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import './mindmate-theme.css'
 import './mindmate-toolbar-theme.css'
 import './hydrationCard.js'
+import './sidebarFeatures.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

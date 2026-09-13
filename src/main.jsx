dd10faae9c297d19b0f7011040a57ea92aeaaa02
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-13 16:03:42 +0530

    Scope journal toolbar to New Entry only

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './mindmate-interaction-fix.js'
 import './stickerToolbarFix.js'
 import './mindmate-final-fix.js'
 import './littleMomentsSection.js'
+import './mindmate-toolbar-scope.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

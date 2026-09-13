8b6a9dccd088274894967a3a4ed6e34a5fc732c6
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-13 19:16:12 +0530

    Add isolated Hydration Check card

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './mindmate-theme.css'
 import './mindmate-toolbar-theme.css'
+import './hydrationCard.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

c6d0820474b7495d6a16c67c0df16fb1ec6d892f
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-13 15:05:26 +0530

    Stabilize login by using clean React startup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './mindmate-final-fix.js'
-import './phaseOne.js'
-import './themeFix.js'
-import './wellnessFix.js'
-import './wellnessPolish.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

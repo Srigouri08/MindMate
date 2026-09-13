dee7c3751e3033cb8394e9833d84ae046782ca83
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-13 18:55:26 +0530

    Apply dark theme to floating toolbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './mindmate-theme.css'
+import './mindmate-toolbar-theme.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

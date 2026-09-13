d22ed5bded94ab4ce985c4896b488dfc6840a5cc
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-13 22:14:46 +0530

    Enable sticker toolbar fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './mindmate-theme.css'
 import './mindmate-toolbar-theme.css'
 import './hydrationCard.js'
 import './sidebarFeatures.js'
+import './stickerToolbarFix.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

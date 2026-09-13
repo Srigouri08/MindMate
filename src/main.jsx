68a7e58dac9c07b8baae8b1cfc77a0cc716f176d
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-13 22:21:42 +0530

    Enable creative tools fixes

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './mindmate-toolbar-theme.css'
 import './hydrationCard.js'
 import './sidebarFeatures.js'
 import './stickerToolbarFix.js'
+import './creativeToolsFix.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

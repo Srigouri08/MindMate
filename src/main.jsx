51273a5c8e3a48c5343d22054b8bda65ff0945de
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-13 13:39:38 +0530

    Add wellness visual polish

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './mindmate-final-fix.js'
 import './phaseOne.js'
 import './themeFix.js'
 import './wellnessFix.js'
+import './wellnessPolish.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

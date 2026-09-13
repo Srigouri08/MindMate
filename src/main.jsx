34855f0837542d0e49e3314eaa0c8d85557058f6
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-13 11:51:50 +0530

    Load theme fixes for feature tools

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './mindmate-final-fix.js'
 import './phaseOne.js'
+import './themeFix.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

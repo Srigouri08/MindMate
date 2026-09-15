107cd16cea44bc4747a1f84c5a9c3b4791170b52
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-15 22:07:39 +0530

    Stabilize Firebase login and authentication UX

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './mindmate-theme.css'
-import './mindmate-toolbar-theme.css'
+import './mindmate-final-fix.js'
+import './phaseOne.js'
+import './themeFix.js'
+import './wellnessFix.js'
+import './wellnessPolish.js'
+import './uxLite.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

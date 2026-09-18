0518ce4fef9c4bcca60f7784409f4037d39b7908
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-18 23:28:46 +0530

    Prevent legacy scripts from blocking MindMate startup

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import './mindmate-theme.css'
-import './mindmate-toolbar-theme.css'
-import './responsive-fixes.css'
-import './responsive-final.css'
-import './hydrationCard.js'
-import './sidebarFeatures.js'
-import './stickerToolbarFix.js'
-import './creativeToolsFix.js'
-import './creativePolishFix.js'
-import './mindmate-screen-toolbar.js'
-import './journalEnhancements.js'
-import './stickerControlsInteractionFix.js'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./mindmate-theme.css";
+import "./mindmate-toolbar-theme.css";
+import "./responsive-fixes.css";
+import "./responsive-final.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);

b4d997f4ca0ea842fbfa691af26d6c5b69ee10a6
Srigouri Siddani <219653524+Srigouri08@users.noreply.github.com>
2026-09-18 23:46:06 +0530

    Restore hydration mood calendar and sidebar creative features

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,9 @@ import "./mindmate-screen-toolbar.js";
 import "./stickerToolbarFix.js";
 import "./stickerControlsInteractionFix.js";
 import "./creativeToolsFix.js";
+import "./creativePolishFix.js";
+import "./hydrationCard.js";
+import "./sidebarFeatures.js";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
